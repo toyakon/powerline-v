e4b5a4d505c3883cc3148d6659808a506d6db45e
toyakon <38478594+toyakon@users.noreply.github.com>
2019-12-11 05:58:38 +0900

    ADD git segment
    Add stash, conflict view.
    Change master bg

--- seg_git_branch.v
+++ seg_git_branch.v
@@ -18,11 +18,18 @@ fn get_branch() string{
 }
 
 fn seg_git_branch(arg Arg) Segment {
+    branch := get_branch()
+    mut bg := theme.git_branch_bg
+    mut fg := theme.git_branch_fg
+    if branch == "master" {
+        bg = theme.git_master_bg
+        fg = theme.git_master_fg
+    }
     return Segment {
         name: "git_branch"
-        content: get_branch()
+        content: branch
         space: true
-        bg: theme.git_branch_bg
-        fg: theme.git_branch_fg
+        bg: bg
+        fg: fg
     }
 }
